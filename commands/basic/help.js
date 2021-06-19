8a94c81ca4ac0493d06ff837cd5d08268ebe29cf
Kìng <77362066+CxtrollerFN@users.noreply.github.com>
2021-06-19 12:24:09 -0400

    Update help.js

--- commands/basic/help.js
+++ commands/basic/help.js
@@ -6,6 +6,8 @@ module.exports = {
       
       let embed = new discord.MessageEmbed()
       .setAuthor("Commands")
+      .addField("> Admin", '`addrole`, `ban`, `kick`, `lockchannel`, `unlockchannel`, `mute`, `unmute`, `unban`, `warn`, `unwarn`, `warnings`, `purge`')
+      .addField("> Basic", '`help`, `uptime`')
       .addField("> Monitor",'`Monitor`,`Remove`,`Stats`')
 			.setFooter('Made by team incasx')
 			.setColor("GREEN");
